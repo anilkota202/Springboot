06a4f1b1b5f267bf5e358c20c0e4af4ec45b1273
ani <ani@192.168.56.1>
2019-12-28 23:40:05 -0800

    ip address

--- src/main/java/com/talcott/talcott/controller/StudentController.java
+++ src/main/java/com/talcott/talcott/controller/StudentController.java
@@ -2,6 +2,8 @@ package com.talcott.talcott.controller;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.talcott.talcott.ipTest.IPAdressTrack;
 import com.talcott.talcott.model.Student;
 import com.talcott.talcott.repository.StudentRepo;
 
@@ -16,13 +19,18 @@ import com.talcott.talcott.repository.StudentRepo;
 
 @RestController 
 public class StudentController {
+	
+	@Autowired
+	private IPAdressTrack IPAdressTrack;
 	 
 	@Autowired
 	private StudentRepo studentrepo;
 	
 	@GetMapping("/stfindall")
-	public List<Student> stmethod()
+	public List<Student> stmethod(HttpServletRequest request)
 	{
+		IPAdressTrack.getClientIp();
+		
 		return studentrepo.findAll();
 	}
 	@GetMapping("/stfindid/{id}")
