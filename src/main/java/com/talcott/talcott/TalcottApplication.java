c15d9e8a711bb917b59e735c237316eedbf8474a
anil <anil@host.docker.internal>
2020-08-31 23:22:23 +0530

    Zipkin added

--- src/main/java/com/talcott/talcott/TalcottApplication.java
+++ src/main/java/com/talcott/talcott/TalcottApplication.java
@@ -1,16 +1,22 @@
 package com.talcott.talcott;
 
 import org.springframework.beans.factory.annotation.Value;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 
+import brave.Tracer;
+import brave.Tracing;
+import brave.sampler.Sampler;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
 
 @SpringBootApplication(scanBasePackages = "com.talcott.* ")
+@EnableEurekaClient
+@EnableDiscoveryClient
 public class TalcottApplication {
 
 	public static void main(String[] args) {
@@ -28,5 +34,14 @@ public class TalcottApplication {
 						.license(new License().name("Apache 2.0").url("http://springdoc.org")));
 
 	}
+	
+	@Bean
+	  public Tracer tracer() {
+	    Tracing tracing = Tracing.newBuilder().build();
+	    tracing.tracer().startScopedSpan("TestSpan");
+	   
+	    return tracing.tracer();
+	  }
+	
 
 }
